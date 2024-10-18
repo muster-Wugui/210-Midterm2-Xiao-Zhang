f94dc31e24bd444bc9ec491cf1c05ef1a8d3c1fa
muster-Wugui <tintinpost28@gmail.com>
2024-10-18 10:43:57 -0700

    Initial Commit

---
+++ Midterm Exam 2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Midterm Exam 2
+//
+//  Created by Xiao Zhang on 10/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
